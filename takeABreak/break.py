578a2ae0093d9b6143c712160ea04ffd71b5677a
Edvan Macedo <edvan.macedo.jr@gmail.com>
2016-12-22 13:14:53 -0400

    Implement call three times of webbrowser

--- takeABreak/break.py
+++ takeABreak/break.py
@@ -1,6 +1,11 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+total_break = 3;
+break_count = 0;
+
+while(break_count < total_break):
+    time.sleep(10)
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    break_count+=1
 
