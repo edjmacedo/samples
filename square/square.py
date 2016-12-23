ddb74050c8510f3496ce4be771808b1603750d05
Edvan Macedo <edvan.macedo.jr@gmail.com>
2016-12-23 10:27:07 -0400

    python: added other shapes like circle and triangle

--- square/square.py
+++ square/square.py
@@ -1,31 +1,40 @@
 import turtle
 
-def draw_square():
+def create_window():
     window = turtle.Screen()
     window.bgcolor("red")
+    draw_square()
+    draw_circle()
+    draw_triangle()
+    window.exitonclick()
 
+def draw_square():
     brad = turtle.Turtle()
     brad.shape("circle")
     brad.color("white")
     brad.speed(5)
 
-    quare.pyrad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    square_side = 0
+    while square_side < 4:
+        brad.right(90)
+        brad.forward(100)
+        square_side += 1
 
+def draw_circle():
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
 
-    window.exitonclick()
+def draw_triangle():
+    trian = turtle.Turtle()
+    trian.shape("triangle")
+    trian.color("yellow")
 
-draw_square()
+    triangle_side = 0
+    while triangle_side < 3:
+        trian.forward(100)
+        trian.right(120)
+        triangle_side += 1
 
-
-
+create_window()
