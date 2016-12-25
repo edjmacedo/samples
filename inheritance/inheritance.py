3f9fe891f71a7fe8d7aa208840e596f927f0b5c6
Edvan Macedo <edvan.macedo.jr@gmail.com>
2016-12-25 17:55:46 -0400

    python: override example

--- inheritance/inheritance.py
+++ inheritance/inheritance.py
@@ -4,14 +4,22 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
         
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+        
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructor Called")
         Parent.__init__(self, last_name, eye_color)        
         self.number_of_toys = number_of_toys
         
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+        print("Number of toys - "+str(self.number_of_toys))
+        
 #bylly_cirus = Parent("Cyrus", "Blue")
 
 miley_cirus = Child("Cyrus", "Blue", 5)
-print(miley_cirus.last_name)
-print(miley_cirus.number_of_toys)
+miley_cirus.show_info()
