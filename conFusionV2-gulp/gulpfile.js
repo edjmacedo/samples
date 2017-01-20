e3a6be9a34f25a3d7155aae4ac4ab96a5625ddd3
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-01-20 16:16:25 -0400

    Added implementation of single page application

--- conFusionV2-gulp/gulpfile.js
+++ conFusionV2-gulp/gulpfile.js
@@ -55,6 +55,8 @@ gulp.task('copyfonts', ['clean'], function() {
    .pipe(gulp.dest('./dist/fonts'));
    gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
    .pipe(gulp.dest('./dist/fonts'));
+   gulp.src('./app/*.html')
+   .pipe(gulp.dest('./dist/'));
 });
 
 // Watch
