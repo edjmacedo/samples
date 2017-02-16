4a315dc2323745290028f7f5f00ca16a11e54d77
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-02-15 23:52:51 -0400

    Added simple list implementation

--- poc_forms/hello_world/main.py
+++ poc_forms/hello_world/main.py
@@ -4,8 +4,24 @@ form_html = """
 <form>
 <h2>Add a Food</h2>
 <input type="text" name="food">
+%s
 <button>Add</button>
 </form>
+"""
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+item_html= "<li>%s</li>"
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
 """
 
 class Handler(webapp2.RequestHandler):
@@ -13,8 +29,21 @@ class Handler(webapp2.RequestHandler):
         self.response.out.write(*a, **kw)
         
 class MainPage(Handler):
-    def get(self):        
-        self.write(form_html)
+    def get(self):      
+        output = form_html
+        output_hidden = ""
+        items = self.request.get_all("food")
+        if items:
+            output_items = ""
+            for item in items:
+                output_hidden += hidden_html % item
+                output_items += item_html % item
+            
+            output_shopping = shopping_list_html % output_items
+            output += output_shopping
+            
+        output = output % output_hidden
+        self.write(output)
 
 
 app = webapp2.WSGIApplication([
