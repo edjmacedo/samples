2f4f59d8d275b94540f7f113682a741900a92c94
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-02-16 23:46:43 -0400

    Added implementation of template inheritance

--- poc_forms/hello_world/main.py
+++ poc_forms/hello_world/main.py
@@ -3,22 +3,8 @@ import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
-
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html= "<li>%s</li>"
-
-shopping_list_html = """
-<br>
-<br>
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):        
@@ -32,27 +18,17 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
         
 class MainPage(Handler):
-    def get(self):     
-        n = self.request.get("n")
-        if n:
-            n = int(n)
-        self.render("shopping_list.html", n=n)
-        ##output = form_html
-        #output_hidden = ""
-        #items = self.request.get_all("food")
-        #if items:
-        #    output_items = ""
-        #    for item in items:
-        #        output_hidden += hidden_html % item
-        #        output_items += item_html % item
-            
-        #    output_shopping = shopping_list_html % output_items
-        #    output += output_shopping
-            
-        #output = output % output_hidden
-        #self.write(output)
+    def get(self):
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)    
 
+class FizzBuzzHandler(Handler):
+    def get(self):
+        n = self.request.get('n', 0)
+        n = n and int(n)
+        self.render('fizzbuzz.html', n = n)           
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),    
+    ('/fizzbuzz', FizzBuzzHandler),
 ], debug=True)
