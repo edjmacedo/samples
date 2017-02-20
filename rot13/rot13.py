0c8e696065cb2a5a6ecce80590c7db64fd061368
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-02-19 22:23:53 -0400

    added implementadion of Rot13 Algotithm using gcloud python

---
+++ rot13/rot13.py
@@ -0,0 +1,32 @@
+import os
+import jinja2
+import webapp2
+import codecs
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):        
+        self.response.out.write(*a, **kw)
+    
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+        
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+        
+class MainPage(Handler):
+    def get(self):        
+        self.render("rot13.html")    
+        
+    def post(self): 
+        text = self.request.get('text')
+        text = codecs.encode(text, 'rot_13')
+        self.render("rot13.html", text = text)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
