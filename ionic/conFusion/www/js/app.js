f696b429a8c4c8a6fc4f7132e2da55943a66d44b
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-03-13 09:52:58 -0400

    Update application to use ionic router

--- ionic/conFusion/www/js/app.js
+++ ionic/conFusion/www/js/app.js
@@ -1,47 +1,40 @@
-// Ionic Starter App
+angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.services'])
-
-.run(function($ionicPlatform, $rootScope, $ionicLoading) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+.run(function ($ionicPlatform, $rootScope, $ionicLoading) {
+  $ionicPlatform.ready(function () {
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
-  });
-  $rootScope.$on('loading:show', function () {
-        $ionicLoading.show({
-            template: '<ion-spinner></ion-spinner> Loading ...'
-        })
-  });
 
-  $rootScope.$on('loading:hide', function () {
-        $ionicLoading.hide();
-  });
+    $rootScope.$on('$stateChangeStart', function () {
+      console.log('Loading ...');
+      $rootScope.$broadcast('loading:show');
+    });
 
-  $rootScope.$on('$stateChangeStart', function () {
-        console.log('Loading ...');
-        $rootScope.$broadcast('loading:show');
-  });
+    $rootScope.$on('$stateChangeSuccess', function () {
+      console.log('done');
+      $rootScope.$broadcast('loading:hide');
+    });
 
-  $rootScope.$on('$stateChangeSuccess', function () {
-        console.log('done');
-        $rootScope.$broadcast('loading:hide');
-  });    
+    $rootScope.$on('loading:show', function () {
+      $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner> Loading ...'
+      });
+    });
+
+    $rootScope.$on('loading:hide', function () {
+      $ionicLoading.hide();
+    });
+
+  });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
 
     .state('app', {
@@ -56,56 +49,66 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
     views: {
       'mainContent': {
         templateUrl: 'templates/home.html',
-        controller: 'IndexController'
+        controller: 'IndexController',
+        resolve: {
+          dish: ['menuFactory', function (menuFactory) {
+            return menuFactory.get({
+              id: 0
+            });
+          }],
+          promotion: ['promotionFactory', function (promotionFactory) {
+            return promotionFactory.get({
+              id: 0
+            });
+          }],
+          corporate: ['corporateFactory', function (corporateFactory) {
+            return corporateFactory.get({
+              id: 3
+            });
+          }]
+        }
       }
     }
   })
 
   .state('app.aboutus', {
-      url: '/aboutus',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/aboutus.html',
-          controller: 'AboutController'
+    url: '/aboutus',
+    views: {
+      'mainContent': {
+        templateUrl: 'templates/aboutus.html',
+        controller: 'AboutController',
+        resolve: {
+          corporate: ['corporateFactory', function (corporateFactory) {
+            return corporateFactory.query();
+          }]
         }
       }
-    })
-    .state('app.contactus', {
-      url: '/contactus',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/contactus.html',          
-        }
+    }
+  })
+
+  .state('app.contactus', {
+    url: '/contactus',
+    views: {
+      'mainContent': {
+        templateUrl: 'templates/contactus.html'
       }
-    })
+    }
+  })
 
   .state('app.menu', {
     url: '/menu',
     views: {
       'mainContent': {
         templateUrl: 'templates/menu.html',
-        controller: 'MenuController'
+        controller: 'MenuController',
+        resolve: {
+          dishes: ['menuFactory', function (menuFactory) {
+            return menuFactory.query();
+          }]
+        }
       }
     }
   })
-  
-  .state('app.favorites', {
-      url: '/favorites',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/favorites.html',
-            controller:'FavoritesController',
-          resolve: {
-              dishes:  ['menuFactory', function(menuFactory){
-                return menuFactory.query();
-              }],
-                            favorites: ['favoriteFactory', function(favoriteFactory) {
-                  return favoriteFactory.getFavorites();
-              }]
-          }
-        }
-      }
-    })
 
   .state('app.dishdetails', {
     url: '/menu/:id',
@@ -114,12 +117,35 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
         templateUrl: 'templates/dishdetail.html',
         controller: 'DishDetailController',
         resolve: {
-            dish: ['$stateParams','menuFactory', function($stateParams, menuFactory){
-                return menuFactory.get({id:parseInt($stateParams.id, 10)});
-            }]
+          dish: ['$stateParams', 'menuFactory', function ($stateParams, menuFactory) {
+            return menuFactory.get({
+              id: parseInt($stateParams.id, 10)
+            });
+          }]
         }
       }
     }
-  });  // if none of the above states are matched, use this as the fallback
+  })
+
+  .state('app.favorites', {
+    url: '/favorites',
+    views: {
+      'mainContent': {
+        templateUrl: 'templates/favorites.html',
+        controller: 'FavoritesController',
+        resolve: {
+          dishes: ['menuFactory', function (menuFactory) {
+            return menuFactory.query();
+          }],
+          favorites: ['favoriteFactory', function (favoriteFactory) {
+            return favoriteFactory.getFavorites();
+          }]
+        }
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
+
 });
