3aac0a040eed8bb4864888b6f354b64efa5c2802
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-12-06 23:39:02 -0400

    Add new york times API

--- sample-ajax-project/js/script.js
+++ sample-ajax-project/js/script.js
@@ -20,6 +20,24 @@ function loadData() {
   
   var streetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + address + '';
   $body.append('<img class="bgimg" src="' + streetViewURL + '">');
+  
+  //NY Time request
+  var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=e1e5f035dbf04500876171dad06b11b4';
+  $.getJSON(nytimesURL, function(data) {
+    $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+    
+    articles = data.response.docs;
+    for (var i = 0; i < articles.length; i++) {
+      var article = articles[i];
+      $nytElem.append('<li class="articles">' +
+                        '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                        '<p>' + article.snippet + '</p>' +
+                      '</li>'
+                     );
+    }
+  }).error(function(e) {
+    $nytHeaderElem.text('New York Times Articles Could not be Loaded');
+  });
 
   return false;
 };
