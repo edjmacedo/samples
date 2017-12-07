758645280f1bd97806a09dd59f7ea2c4a86c3ed2
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-12-06 23:21:53 -0400

    Added google street view API to get images

--- sample-ajax-project/js/script.js
+++ sample-ajax-project/js/script.js
@@ -1,21 +1,27 @@
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  var $body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
+  var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+  // clear out old data before new request
+  $wikiElem.text("");
+  $nytElem.text("");
 
-    // load streetview
+  // load streetview
+  var streetStr = $("#street").val();
+  var cityStr = $("#city").val();
+  var address = streetStr + ', ' + cityStr;
+  // YOUR CODE GOES HERE!
+  $greeting.text("So, you want to live at " + address + '?');
+  
+  var streetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + address + '';
+  $body.append('<img class="bgimg" src="' + streetViewURL + '">');
 
-    // YOUR CODE GOES HERE!
-
-    return false;
+  return false;
 };
 
 $('#form-container').submit(loadData);
