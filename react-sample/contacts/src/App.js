9c439d73416e87394dbf5a5514843c42daee04a5
Edvan Macedo <edvan.macedo.jr@gmail.com>
2018-02-08 15:03:50 -0400

    Implement simple sample contact list with react

--- react-sample/contacts/src/App.js
+++ react-sample/contacts/src/App.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts;
-    
-    return <ol>
-      {people.map(person => (
-       <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Edvan' },
-          { name: 'Kukinha' },
-          { name: 'Mili' },
-          { name: 'Lisandra' }
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Tyler' },
-          { name: 'Ryan' },
-          { name: 'Geoff' }
-        ]}/>
+      <div>
+        <ListContacts contacts={contacts} />
       </div>
-    );
+    )
   }
 }
 
