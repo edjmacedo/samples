8c5b09c66d1788d698b181d7c887e18da16662bc
Edvan Macedo <edvan.macedo.jr@gmail.com>
2018-02-20 16:39:26 -0400

    Implement contact creation

--- react-sample/contacts/src/App.js
+++ react-sample/contacts/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListContacts from './ListContacts';
-import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
+    screen: 'list',
     contacts: []
   }
-  
+
   componentDidMount() {
     ContactsAPI.getAll().then((contacts) => {
       this.setState({ contacts })
@@ -19,11 +22,35 @@ class App extends Component {
     }))
     ContactsAPI.remove(contact);
   }
-  
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+        <Route exact path="/" render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+            onNavigate={() => {
+              this.setState({ screen: 'create' })
+            }}
+          />
+        )} />
+        <Route path="/create" render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push("/")
+            }}
+          />
+        )}/>
       </div>
     )
   }
